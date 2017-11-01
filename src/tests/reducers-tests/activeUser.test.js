df44315dcf22766d453d6480f90a44faa4c778c8
Nik Bornemeier <NikolausRB@gmail.com>
2017-11-01 17:51:19 -0600

    write activeUser reducer tests

---
+++ src/tests/reducers-tests/activeUser.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import activeUser from '../../reducers/activeUser.js';
+import mockUser from '../../Utils/mockData.js'
+
+
+describe('activeUser', () => {
+
+  it('should return a default state of NULL and also NULL on sign out', () => {
+    const expectation = null;
+    expect(activeUser(undefined, {})).toEqual(expectation)
+  })
+
+  it('shold add an active user to State', () => {
+    const expectation = mockUser;
+    const action = {type: 'SET_ACTIVE_USER', mockUser}
+    expect(activeUser(undefined, action)).toEqual(expectation)
+  })
+  
+})
