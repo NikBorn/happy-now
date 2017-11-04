a134f3edb0c037ff5875fc16ee832ea4a41b448d
Nik Bornemeier <NikolausRB@gmail.com>
2017-11-04 12:01:08 -0600

    add mock store to header-test to make it pass again

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { shallow, mount, configure } from 'enzyme';
 import Header from '../Components/Header/Header.js';
 import Adapter from 'enzyme-adapter-react-16';
+import configureStore from 'redux-mock-store';
+
 
 configure({ adapter: new Adapter() });
 
 describe('Header', () => {
-
-  let wrapper = shallow(<Header/>);
+  const mockStore = configureStore();
+  const initialState = {
+    locations: [],
+    userLocation: {},
+    activeUser: null,
+    count: 10,
+    favorites: []
+  };
+  const store = mockStore(initialState);
+  let wrapper = shallow(<Header store={store}/>);
 
   it('should create an instance of a card', () => {
     expect(wrapper).toMatchSnapshot();
